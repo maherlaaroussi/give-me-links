eea9d6c40a60d9842f6436222713a297dd9f018b
maherlaaroussi <maher.laaroussi@gmail.com>
2020-04-16 00:38:35 +0200

    We're still learning

--- gml/app.py
+++ gml/app.py
@@ -1,2 +1,13 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import csv
+
+site = 'https://www2.zone-warez.com/films-bluray-hd-1080/'
+hdr = {'User-Agent': 'Mozilla/5.0'}
+
 def run():
     print("Here we go!!")
+    req = Request(site,headers=hdr)
+    page = urlopen(req)
+    soup = BeautifulSoup(page)
+    print(soup)
