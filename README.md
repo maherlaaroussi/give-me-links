fc580fc4e0d05a6d24309a5103ea5260d74163d8
maherlaaroussi <maher.laaroussi@gmail.com>
2020-05-12 02:07:21 +0200

    Completly reviewed the code and technologies used

--- README.md
+++ README.md
@@ -7,10 +7,15 @@
   ```bash
   python3 -m venv env
   source ./env/bin/activate
-  python -m pip install beautifulsoup4
+  python -m pip install beautifulsoup4 selenium pandas
   ```
 
-2. Run the package
+2. Geckodriver
+
+  1. Download : <https://github.com/mozilla/geckodriver/releases>
+  2. Copy geckodriver in /usr/local/bin
+
+3. Run the package
 
   ```bash
   python3 -m gml
--- gml/app.py
+++ gml/app.py
@@ -1,13 +1,28 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import csv
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+import pandas as pd
+import re
+import os
 
-site = 'https://www2.zone-warez.com/films-bluray-hd-1080/'
-hdr = {'User-Agent': 'Mozilla/5.0'}
+website = 'https://www2.tirexo.com/films-bluray-hd-1080/'
+waiting_time = 25
 
 def run():
     print("Here we go!!")
-    req = Request(site,headers=hdr)
-    page = urlopen(req)
-    soup = BeautifulSoup(page)
-    print(soup)
+    browser = webdriver.Firefox()
+    browser.get(website)
+    browser.implicitly_wait(waiting_time)
+    print("Getting links...\n\n")
+    movies = browser.find_elements_by_xpath('//*[@id="dle-content"]/div[1]')
+    movies_links = browser.find_elements_by_xpath('/html/body/div[1]/div/div/div/div[2]/div/div/a[2]')
+    
+    # TODO: Récupérer les links à part parce les élements sont liées à la page
+    for i in movies_links:
+        browser.get(i.get_attribute("href"))
+        browser.implicitly_wait(waiting_time)
+
+    browser.quit()
