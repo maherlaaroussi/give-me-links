afb2349b877b4e8ce726c62af824853ccf93fec0
maherlaaroussi <maher.laaroussi@gmail.com>
2020-05-12 15:28:26 +0200

    Update app.py

--- gml/app.py
+++ gml/app.py
@@ -15,11 +15,11 @@ website = 'https://www2.tirexo.com/films-bluray-hd-1080/'
 waiting_time = 25
 
 def run():
+    options = Options()
+    options.add_argument('--headless')
     browser = webdriver.Firefox(options=options)
     try:
         print("Here we go!!")
-        options = Options()
-        options.add_argument('--headless')
         browser.get(website)
         browser.implicitly_wait(waiting_time)
         # Getting pages's links of movies
